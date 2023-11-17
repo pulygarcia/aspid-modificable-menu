d3f1b3903934c7ffc33ea176c2d6637a1367952f
puly garcia <pulygarcia09@gmail.com>
2023-11-17 10:23:20 -0300

    submit color changed, some styles in admin header was changed too

--- formkit.config.js
+++ formkit.config.js
@@ -23,7 +23,7 @@ const config = {
               fileItem: 'hidden'
           },
           submit:{
-              input: '$reset bg-blue-700 py-2 w-full mt-5 font-bold text-white hover:bg-blue-800 transition disabled:opacity-50'
+              input: '$reset bg-red-500 py-2 w-full mt-5 font-bold text-white hover:bg-red-800 transition disabled:opacity-50'
           }
       })
   }
