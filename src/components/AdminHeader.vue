8909ce052c42393cc0144d36ee8f6688948cd051
puly garcia <pulygarcia09@gmail.com>
2023-11-23 10:38:30 -0300

    wrapping links for better ux

--- src/components/AdminHeader.vue
+++ src/components/AdminHeader.vue
@@ -92,24 +92,32 @@
                         </svg>
                     </div>
                     <ul class="flex flex-col text-right mt-8 space-y-4">
-                        <li class="text-sm font-semibold bg-[#efefef] rounded p-1 flex items-center justify-center gap-1" @click="isOpenMenu = false">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
-                            </svg>
-                            <RouterLink :to="{name: 'home'}">Volver al inicio</RouterLink>
-                        </li>
-                        <li class="text-sm font-semibold bg-[#efefef] rounded p-1 flex items-center justify-center gap-1" @click="isOpenMenu = false">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
-                            </svg>
-                            <RouterLink :to="{name: 'current-menu'}">Carta actual</RouterLink>
-                        </li>
-                        <li class="text-sm font-semibold bg-[#efefef] rounded p-1 flex items-center justify-center gap-1" @click="isOpenMenu = false">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                            </svg>
-                            <RouterLink :to="{name: 'new-item'}">Agregar al menú</RouterLink>
-                        </li>
+                        <RouterLink :to="{name: 'home'}" class="text-sm font-semibold bg-[#efefef] rounded p-1 flex items-center justify-center gap-1" @click="isOpenMenu = false">
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                                </svg>
+                            </li>
+                            <span>Volver al inicio</span>
+                        </RouterLink>
+
+                        <RouterLink :to="{name: 'current-menu'}" class="text-sm font-semibold bg-[#efefef] rounded p-1 flex items-center justify-center gap-1" @click="isOpenMenu = false">
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+                                </svg>
+                            </li>
+                            <span>Carta actual</span>
+                        </RouterLink>
+
+                        <RouterLink :to="{name: 'new-item'}" class="text-sm font-semibold bg-[#efefef] rounded p-1 flex items-center justify-center gap-1" @click="isOpenMenu = false">
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                </svg>
+                            </li>
+                            <span>Agregar al menú</span>
+                        </RouterLink>
 
                         <button
                             v-if="authStore.currentUser"
